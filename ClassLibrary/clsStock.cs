bf20a43019723c333c77ce9c27963a488062e57a
Venketesh Prabhudas <P2588086@my365.dmu.ac.uk>
2022-02-23 14:08:50 +0000

    Week 21 work

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary; 
+
 
 namespace ClassLibrary
 {
-    class clsStock
+    public class clsStock
     {
+        public string ProductCategory; 
+
     }
 }
