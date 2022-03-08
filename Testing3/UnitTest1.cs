83e1973c739b5ab62591976a5e21ecac7a0812f3
P2603057 <P2603057@my365.dmu.ac.uk>
2022-03-08 15:09:04 +0000

    Week 20 work- creating test cases

--- Testing3/UnitTest1.cs
+++ Testing3/UnitTest1.cs
@@ -10,10 +10,68 @@ namespace Testing3
         [TestMethod]
         public void InstanceOK()
         {
-            clsSupplier ASupplier = new clsSupplier();
+            clssupplier ASupplier = new clssupplier();
             Assert.IsNotNull(ASupplier);
         }
-        
+
+        // ======================================================================
+
+        [TestMethod]
+        public void SupplierSinceOK()
+        {
+            clssupplier ASupplier = new clssupplier();
+            string SupplierSince = "12/12/22";
+            ASupplier.SupplierSince = SupplierSince;
+            Assert.AreEqual(ASupplier.SupplierSince, "12/12/22");
+        }
+
+        // ======================================================================
+
+        [TestMethod]
+        public void SupplierNameOK()
+        {
+            clssupplier ASupplier = new clssupplier();
+            string SupplierName = "Nike";
+            ASupplier.SupplierName = SupplierName;
+            Assert.AreEqual(ASupplier.SupplierName, "Nike");
+        }
+
+        // ======================================================================
+
+        [TestMethod]
+        public void CurrentSupplierOK()
+        {
+            clssupplier ASupplier = new clssupplier();
+            Boolean CurrentSupplier = true;
+            ASupplier.CurrentSupplier = CurrentSupplier;
+            Assert.AreEqual(ASupplier.CurrentSupplier, true);
+        }
+
+        // ======================================================================
+
+        [TestMethod]
+        public void SupplierAddressOK()
+        {
+            clssupplier ASupplier = new clssupplier();
+            string SupplierAddress = "test address";
+            ASupplier.SupplierAddress = "test address";
+            Assert.AreEqual(ASupplier.SupplierAddress, "test address");
+        }
+
+        // ======================================================================
+
+        [TestMethod]
+        public void SupplierContactNumberOK()
+        {
+            clssupplier ASupplier = new clssupplier();
+            long TestData = 7647766891;
+            ASupplier.ContactNumber = TestData;
+            Assert.AreEqual(ASupplier.ContactNumber, TestData);
+        }
 
     }
 }
+
+
+
+          
