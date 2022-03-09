65d4150155ce36c708a0b37dc9b063c5525908ea
Rida Koultouma <m.rida-koultouma@hotmail.com>
2022-03-09 17:38:02 +0000

    caught up to week 23

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -5,16 +5,25 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
-
+      
        
         public string staffFullname;
         public int staffId;
-        public string startDate;
-        public string availability;
+        public DateTime startDate;
+        public bool availability;
         public string staffRole;
         public string CustomerID;
         public object tbstaffFullname;
 
-        public string StaffFullname { get; set; }
+       
+public bool Find(int staffId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Find(object staffId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
