037d1e1d5a9f6052f1e69466a385757e278ad0f8
Frederick Palmer <P2601945@my365.dmu.ac.uk>
2022-03-19 14:51:55 +0000

    week 24

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -8,16 +8,98 @@ namespace ClassLibrary
 {
     public class clsCustomer
     {
-
         public string CustomerFirstname;
         public string CustomerSurnamename;
         public string CustomerEmail;
-        public string CustomerDOB;
-        public Boolean Gender;
-        public DateTime dateTime;
-        public int staffID;
-        public int customerId;
+        public string Gender;
+        public DateTime CustomerDOB;
+        public int CustomerId;
+
+        private Int32 mCustomerID;
+
+        public int CustomerID
+        {
+
+            get
+            {
+
+                return mCustomerID;
+            }
+            set
+            {
+                mCustomerID = value;
+            }
+
+        }
+        private string mCustomerFirstname;
+        public string CustomerFirtname
+        {
+
+            get
+            {
+                return mCustomerFirtname;
+
+            }
+            set
+            {
+                mCustomerFirstname = value;
+            }
+        }
+    }
+    private string mCustomerSurname;
+    public string CustomerSurname
+    {
+
+        get
+        {
+            return mCustomerSurname;
+
+        }
+        set
+        {
+            mCustomerSurname = value;
+        }
+    }
+    private string mCustomerEmail;
+    public string CustomerEmail
+    {
+
+        get
+        {
+            return mCustomerEmail;
+
+        }
+        set
+        {
+            mCustomerEmail = value;
+        }
     }
 
-        
+
+    private DateTime mCustomerDOB;
+    public DateTime CustomerDOB
+    {
+        get
+        {
+            return mCustomerDOB;
+        }
+        set
+        {
+            mCustomerDOB = value;
+        }
+    }
+
+    private string mGender;
+    public string Gender
+    {
+        get
+        {
+            return mGender;
+        }
+        set
+        {
+            mGender = value;
+        }
+    }
+
 }
