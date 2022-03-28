1947c67f893b311c99c6beb8fc6c1ee04d9b2b52
Rida Koultouma <m.rida-koultouma@hotmail.com>
2022-03-28 16:08:28 +0100

    nearly done week 24

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections;
 
+
+
 namespace ClassLibrary
 {
     public class clsStaff
     {
-      
-       
+
+
         public string staffFullname;
         public int staffId;
         public DateTime startDate;
@@ -15,8 +17,8 @@ namespace ClassLibrary
         public string CustomerID;
         public object tbstaffFullname;
 
-       
-public bool Find(int staffId)
+
+        public bool Find(int staffId)
         {
             throw new NotImplementedException();
         }
@@ -26,4 +28,130 @@ public bool Find(int staffId)
             throw new NotImplementedException();
         }
     }
+    public class clsStaff
+    {
+
+
+
+        private int mstaffId;
+
+
+        public int staffId
+
+        {
+
+            get
+        }
+
+        return mstaffId
+            }
+    set
 }
+
+        //set the private data
+        mstaffId = value;
+}
+
+}
+
+
+//private date added 
+private DateTime mstartDate;
+
+//public for when started
+public DateTime startDate;
+{
+    get
+    { 
+        return mstartDate;
+
+    }
+
+    set
+
+    {  
+        //set the private data
+        mstartDate = value;
+    }
+
+}
+
+
+
+public string staffFullname
+
+{
+    get
+    {
+        return mstaffFullname;
+
+    }
+
+    set
+
+    {
+
+        mstaffFullname = value;
+
+    }
+
+
+}
+
+private Boolean mavailability;
+
+public bool availability
+
+{
+    get
+}
+
+return mavailability;
+}
+
+set
+{ 
+    mavailability = value;
+
+}
+
+}
+private string mstaffRole;
+
+public string staffRole
+
+{
+    get
+    { 
+
+return mstaffRole;
+}
+set
+
+{
+    mstaffRole = value;
+
+}
+
+}
+public bool Find(int staffId)
+{
+    clsDataConnection DB = new clsDataConnection();
+    DB.AddParameter("@staffId", staffId);
+    DB.Execute("sproc_tblAddress_FilterBystaffId");
+    if (DB.Count == 1)
+    {
+        mstaffId = Convert.ToInt(DB.DataTable.Rows[0]["staffId"]);
+        mstaffFullname = Convert.ToString(DB.DataTable.Rows[0]["staffFullname"]);
+        mstaffRole = Convert.ToString(DB.DataTable.Rows[0]["staffRole"]);
+        mstartDate = Convert.ToDateTime(DB.DataTable.Rows[0]["startDate"]);
+        mavailability = Convert.ToBoolean(DB.DataTable.Rows[0]["availability"])
+            return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
