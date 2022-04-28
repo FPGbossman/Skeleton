423629948d18efc27aa749592491e11a467b6d9e
Frederick Palmer <P2601945@my365.dmu.ac.uk>
2022-04-28 14:33:17 +0100

    This is it

---
+++ Testing2/tstCustomerCollection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary;
+using System.Collections.Generic;
+
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstCustomerCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomerCollection gg = new clsCustomerCollection();
+
+            Assert.IsNotNull(gg);
+        }
+
+    }
+}
